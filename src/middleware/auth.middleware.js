065d49cdc3a1e8b234ab670a2508dfeae273cab4
mishra-pankaj <immishrapankaj@gmail.com>
2026-06-11 16:14:56 +0530

    auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+const userModel = require('../models/user.models');
+const jwt = require('jsonwebtoken');
+
+async function authMiddleware(req, res, next) {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            message: "Token is missing",
+            status: "failed"
+        });
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        });
+        const user = await userModel.findById(decoded.userId);
+
+        req.user = user;
+        return next();
+
+    }
+    catch(err){
+        return res.status(401).json({
+            message: "Unauthorized acces ,Token is invalid",
+            status: "failed"
+        });
+        
+    }
+
+}
